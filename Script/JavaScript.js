07a620e5199e11f80d315603ca237b3672435111
Schaupp-Matthew <ccbsamatt@gmail.com>
2017-10-04 00:03:28 -0400

    testing js
    test

---
+++ Script/JavaScript.js
@@ -0,0 +1,36 @@
+function numGame() {
+	var a = Math.floor((Math.random()*100)+1);
+	var g;
+	numberGuessGame(g,a);
+}
+
+function numberGuessGame(guess,answer) {
+var message = 
+	"I'm thinking of a number between 1 and 100.\n" +
+	"Try to guess it! You have 10 guesses to win!\n" +
+	"Please enter an integer between 1 and 100.";
+ 
+var guess;
+var guessCount = 0;
+
+do {
+	guess = parseInt(window.prompt(message));
+	if (guessCount > 10) {
+		message = "You loose :(";
+		window.alert(message);
+		break;}
+	else if (guess < answer) {
+		message = guess + " is too low. Please enter another integer.";
+		guessCount++;}
+	else if (guess > answer) {
+		message = guess + " is too high. Please enter another integer.";
+		guessCount++}
+} while (guess != answer);
+
+if (guess == answer){
+	message = guess + " is correct!"; 
+	var messageGuess = "Number of guesses: " + guessCount;
+	window.alert(message);
+	window.alert(messageGuess);}
+}
+
