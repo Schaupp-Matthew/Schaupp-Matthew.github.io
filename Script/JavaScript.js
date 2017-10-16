f6c8dff8730137091eb312daa0f2a8a8cb434435
Schaupp-Matthew <ccbsamatt@gmail.com>
2017-10-16 01:20:17 -0400

    final test.2

--- Script/JavaScript.js
+++ Script/JavaScript.js
@@ -262,7 +262,7 @@ function getLocalStorage() {
 		//document.write(x);
 		//document.write(objectK.contacts[x].address);
 		//document.write(objectK.contacts[x].firstname + " / " + objectK.contacts[x].lastname + " / " + objectK.contacts[x].phonenumber + " / " + objectK.contacts[x].address);
-		output += objectK.contacts[x].firstname + " / " + objectK.contacts[x].lastname + " / " + objectK.contacts[x].phonenumber + " / " + objectK.contacts[x].address;
+		output += objectK.contacts[x].firstname + " / " + objectK.contacts[x].lastname + " / " + objectK.contacts[x].phonenumber + " / " + objectK.contacts[x].address + "<br />";
 		document.getElementById('localStorage').innerHTML = output + "<br />" + "End of all of the information that was stored in localStorage.";
 	}
 	//document.write("<br />" + "End of all of the information that was stored in localStorage.");
